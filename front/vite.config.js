a9d9598f13b309e6dfe283c1559298e36bd9bde1
Sbaoor-fly <2582152047@qq.com>
2025-07-18 12:53:46 +0800

    rename backend

--- front/vite.config.js
+++ front/vite.config.js
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 export default defineConfig({
   plugins: [vue()],
   build: {
-    outDir: '../backed/dist'   // ← 改这里
+    outDir: '../backend/dist'   // ← 改这里
   },
   server: {
     proxy: {
